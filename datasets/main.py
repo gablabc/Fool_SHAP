73f9ee63e134002b7fdb844bd9521967e46af866
galabc <gabriel.laberge@polymtl.ca>
2022-02-11 15:04:22 -0500

    Refactor preprocessing scripts.

--- datasets/main.py
+++ datasets/main.py
@@ -1,33 +1,13 @@
 import argparse
-import os
-
-from adult_income import get_adult_income
-from compas import get_compas
-from default_credit import get_default_credit
-from marketing import get_marketing
+from process import DATA_PROCESS
 
 # parser initialization
 parser = argparse.ArgumentParser(description='Script for datasets preprocessing')
-
 parser.add_argument('--dataset', type=str, default='adult_income', help='Dataset: adult_income, compas, default_credit, marketing')
 args = parser.parse_args()
-
-
 dataset = args.dataset 
 
-
-if dataset == 'adult_income':
-    get_adult_income(save_df=True)
-
-elif dataset == 'compas':
-    get_compas(save_df=True)
-    
-elif dataset == 'default_credit':
-    get_default_credit(save_df=True)
-
-elif dataset == 'marketing':
-    get_marketing(save_df=True)
-
+if dataset in DATA_PROCESS.keys():
+    DATA_PROCESS[dataset]()
 else:
     print('This dataset is not handled for the moment')
-
--- datasets/process.py
+++ datasets/process.py
@@ -1,23 +1,23 @@
 import numpy as np
 import pandas as pd
-from core import save
+from sklearn.utils import shuffle
+import os
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
-def get_adult_income(save_df=False):
+def get_adult_income():
 
     # output files
     dataset = 'adult_income'
-    decision = 'income'
 
     raw_data_1 = np.genfromtxt('./raw_datasets/adult_income/adult.data', delimiter=', ', dtype=str)
-
     raw_data_2 = np.genfromtxt('./raw_datasets/adult_income/adult.test', delimiter=', ', dtype=str, skip_header=1)
 
     column_names = ['age', 'workclass', 'fnlwgt', 'education', 'educational-num', 'marital-status', 'occupation', 'relationship', 'race', 'gender', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income']
 
     df_1 = pd.DataFrame(raw_data_1, columns=column_names)
     df_2 = pd.DataFrame(raw_data_2, columns=column_names)
-
     df = pd.concat([df_1, df_2], axis=0)
 
 
@@ -57,7 +57,87 @@ def get_adult_income(save_df=False):
     df = df.rename(columns={'marital-status': 'marital_status', 'hours-per-week': 'hours_per_week', 'capital-gain': 'capital_gain', 'capital-loss': 'capital_loss'})
     #df = df.rename(columns={'marital-status': 'marital_status', 'hours-per-week': 'hours_per_week'})
 
-    if save_df:
-        for rseed in range(10):
-            save(df, dataset, decision, rseed)
-    #return df
+    save(df, dataset)
+
+
+
+def get_marketing():
+    # output files
+    dataset = 'marketing'
+    decision = 'subscribed'
+
+    df = pd.read_csv('./raw_datasets/marketing/marketing.csv')
+    
+    df['age'] = df['age'].apply(lambda x: 'age:30-60' if ((x >= 30) & (x <=60))  else 'age:not30-60')
+    
+
+    save(df, dataset)
+
+
+
+def get_default_credit():
+    # output files
+    dataset = 'default_credit'
+    decision = 'good_credit'
+
+    df = pd.read_csv('./raw_datasets/default_credit/default_credit.csv')
+
+    
+    df['good_credit'] = 1 - df['DEFAULT_PAYEMENT']
+
+    df = df.drop(labels=['DEFAULT_PAYEMENT'], axis = 1)
+
+    df = df.dropna()
+
+    #missing_fractions = df.isnull().mean().sort_values(ascending=False) # Fraction of data missing for each variable
+    #print(missing_fractions[missing_fractions > 0]) # Print variables that are missing data
+
+
+    save(df, dataset)
+
+
+
+
+def get_compas(save_df=False):
+
+    # output files
+    dataset = 'compas'
+
+    """Loads COMPAS dataset from https://raw.githubusercontent.com/algofairness/fairness-comparison/master/fairness/data/raw/propublica-recidivism.csv
+    :param: save_intermediate: save the transformed dataset. Do not save by default.
+    """
+    url = "https://raw.githubusercontent.com/algofairness/fairness-comparison/master/fairness/data/raw/propublica-recidivism.csv"
+
+    df = pd.read_csv(url)
+
+    df['low_risk'] = 1 - df['two_year_recid']
+
+    df = df[['sex', 'age', 'race', 'juv_fel_count', 'juv_misd_count', 'juv_other_count', 'priors_count', 'c_charge_degree', 'low_risk']]
+
+    df.to_csv('./raw_datasets/compas/compas.csv', index=False)
+
+    print(len(df))
+
+    df = df[(df['race']=='African-American') | (df['race']=='Caucasian')]
+
+    print(len(df))
+
+    df = df.replace({'c_charge_degree': {'M': 'Misdemeanor', 'F': 'Felony'}})
+
+    save(df, dataset)
+
+
+
+def save(df, dataset):
+    df = shuffle(df, random_state=99)
+    full_name = os.path.join("preprocessed", f"{dataset}.csv")
+    df.to_csv(full_name, index=False)
+
+
+
+DATA_PROCESS = {
+    'adult_income' : get_adult_income,
+    'default_credit' : get_default_credit,
+    'marketing' : get_marketing,
+    'compas' : get_compas
+}

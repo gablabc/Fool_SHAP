caf1377f44c9252abf35bfd05eddea61799649ec
galabc <gabriel.laberge@polymtl.ca>
2022-11-19 13:03:55 -0500

    Add Genetic to final figure.

--- 3_2_plot_all_results.py
+++ 3_2_plot_all_results.py
@@ -34,6 +34,16 @@ if __name__ == "__main__":
     df["fool_abs_diff"] = 100 * (df["init_abs"] - df["final_abs"]) / df["init_abs"]
     grouped = df.groupby('dataset')
 
+    # genetic results
+    df2 = []
+    for dataset in ["adult_income", "communities", "marketing", "compas"]:
+        for rseed in range(5):
+            filename = os.path.join("attacks", "Genetic", f"rf_{dataset}_rseed_{rseed}.csv")
+            df_genetic = pd.read_csv(filename)
+            ref_abs = df_genetic.loc[0, "loss"]
+            best_abs = df_genetic[df_genetic["detection"]==0]["loss"].min()
+            df2.append([dataset, 100 * (ref_abs - best_abs) / ref_abs, "Genetic"])
+    df2 = pd.DataFrame(df2, columns=["dataset", "diff", "method"])
     # ECDF
     # plt.figure()
     # for i, (name, group) in enumerate(grouped):
@@ -64,12 +74,19 @@ if __name__ == "__main__":
     df_copy = [ pd.DataFrame(np.column_stack((df["dataset"], df["brute_abs_diff"], len(df)*["Brute"])),
                             columns=["dataset", "diff", "method"]),
                 pd.DataFrame(np.column_stack((df["dataset"], df["fool_abs_diff"], len(df)*["Ours"])),
-                            columns=["dataset", "diff", "method"]) ]
+                            columns=["dataset", "diff", "method"]),
+                df2
+                ]
     df_copy = pd.concat(df_copy)
 
+    for old_name, new_name in zip(["adult_income", "compas", "marketing", "communities"],
+                                  ["Adult", "COMPAS", "Marketing", "C\&C"]):
+        df_copy.replace(old_name, new_name, inplace=True)
+    # Rename the datasets for better plot
     # Decreases in relative amplitude
     order = ["COMPAS", "Adult", "Marketing" ,"C\&C"]
-    sns.boxplot(x="dataset", y="diff", hue="method", data=df_copy, width=0.45, order=order)
+    hue_order = ["Brute", "Genetic", "Ours"]
+    sns.boxplot(x="dataset", y="diff", hue="method", data=df_copy, width=0.45, order=order, hue_order=hue_order)
 
     # plt.figure()
     # colors = ['b', 'orange', 'g', 'r']
@@ -78,5 +95,5 @@ if __name__ == "__main__":
     #     plt.step(*get_cdf(group["fool_abs_diff"]), linewidth=2, color=colors[i], where='post', label=name)
     plt.xlabel("Dataset")
     plt.ylabel("Relative Amplitude Decrease (\%)")
-    plt.legend(fontsize=15, framealpha=1, loc="upper right", bbox_to_anchor=(1,0.8))
+    plt.legend(fontsize=15, framealpha=1, loc="upper right", bbox_to_anchor=(1,0.86))
     plt.savefig(os.path.join("Images", f"abs_results.pdf"), bbox_inches='tight')

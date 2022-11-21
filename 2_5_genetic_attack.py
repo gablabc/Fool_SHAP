23302c48caf11f88c5d3164e6e81885ec6e0c04d
galabc <gabriel.laberge@polymtl.ca>
2022-11-21 13:47:42 -0500

    More arguments to genetic attack.

--- 2_5_genetic_attack.py
+++ 2_5_genetic_attack.py
@@ -19,6 +19,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Script for training models')
     parser.add_argument('--dataset', type=str, default='communities', help='Dataset: adult_income, compas, default_credit, marketing')
     parser.add_argument('--model', type=str, default='rf', help='Model: mlp, rf, gbt, xgb')
+    parser.add_argument('--n_ite', type=int, default=100, help='Number of iterations')
+    parser.add_argument('--n_pop', type=int, default=10, help='Size of the population')
     parser.add_argument('--rseed', type=int, default=0, help='Random seed for the data splitting')
     args = parser.parse_args()
 
@@ -53,9 +55,9 @@ if __name__ == "__main__":
     # All background/foreground predictions
     if args.model == "xgb":
         # When explaining Boosted trees with TreeSHAP, we explain the logit
-        f_D_0 = model.predict(D_0, output_margin=True).reshape((-1, 1))
+        f_D_0 = model.predict(ohe_encoder.transform(D_0), output_margin=True).reshape((-1, 1))
         f_S_0 = f_D_0[:200]
-        f_D_1 = model.predict(D_1, output_margin=True).reshape((-1, 1))
+        f_D_1 = model.predict(ohe_encoder.transform(D_1), output_margin=True).reshape((-1, 1))
     else:
         # We explain the probability of class 1
         f_D_0 = model.predict_proba(ohe_encoder.transform(D_0))[:, [1]]
@@ -69,11 +71,11 @@ if __name__ == "__main__":
     tmp_filename = f"{args.model}_{args.dataset}_rseed_{args.rseed}.csv"
 
     alg = GeneticAlgorithm(model, D_0[:200], D_1[:200], f_S_0, f_D_0, f_D_1, s_idx, 
-                            pop_count=5, mutation_with_constraints=False, 
+                            pop_count=args.n_pop, mutation_with_constraints=False, 
                             constant=list(range(len(numerical_features),n_features)),
                             ordinal_encoder=ordinal_encoder, ohe_encoder=ohe_encoder)
     
-    alg.fool_aim(max_iter=50, random_state=0)
+    alg.fool_aim(max_iter=args.n_ite, random_state=0)
 
     # Save logs
     results_file = os.path.join("attacks", "Genetic", tmp_filename)
